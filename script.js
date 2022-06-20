604023a8f99f9b9d3f26b452b084290409892ada
Demandtech <demandwork247@gmail.com>
2022-06-20 17:51:30 +0100

    first Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let form = document.querySelector('form');
+
+
+form.addEventListener("submit", function(e){
+ e.preventDefault();
+ 
+ var email = document.getElementById("email");
+ var text = document.getElementById("error-msg");
+ var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+ var errorImage = document.getElementById("error-img");
+
+  if(pattern.test(email.value)){
+    email.style.borderColor = "green";
+    errorImage.style.display = "none";
+    text.innerHTML = "";
+    return true
+  }
+  else{
+   text.innerHTML = "please provide a valid email";
+   errorImage.style.display = "block";
+   email.style.borderColor = "red";
+  }
+})
